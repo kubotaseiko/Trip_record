be9081a6e685700ef3be0c317c0f1b4f01d147ac
kubotaseiko <kubota712.050@gmail.com>
2021-09-04 08:10:37 +0000

    調整

--- db/migrate/20210827082556_create_tagmaps.rb
+++ db/migrate/20210827082556_create_tagmaps.rb
@@ -1,7 +1,7 @@
 class CreateTagmaps < ActiveRecord::Migration[5.2]
   def change
     create_table :tagmaps do |t|
-      t.references :spot, foreign_key: true
+      t.references :spot, type: :bigint, foreign_key: true
       t.references :tag, foreign_key: true
 
       t.timestamps
